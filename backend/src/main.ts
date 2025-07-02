d230b7113f125db021c3f1a5f6f6d940b4b8b720
Georgekuprashvili <kuprashviligeorge@gmail.com>
2025-07-02 21:06:59 +0400

    test

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -7,12 +7,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: process.env.FRONT_URL,
+    origin: '*',
     credentials: true,
   });
 
   app.use(cookieParser());
-  
 
   app.useGlobalPipes(
     new ValidationPipe({
